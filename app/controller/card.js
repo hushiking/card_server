f6e44a457fc503323f26931771c52d462ef66cd5
hushijin <hushijin@meiqia.com>
2018-03-15 01:09:21 +0800

    fix: modify saveCardAction

--- app/controller/card.js
+++ app/controller/card.js
@@ -39,9 +39,13 @@ module.exports = class extends controller {
     }
     async saveCardAction () {
         let cardData = this.param();
+        let openid = this.param('openid');
+        let userData = await this.useModel.where({openid: openid}).find();
         console.log(cardData);
         cardData.support = [];
         cardData.comment = [];
+        cardData.nickname = userData.nickname;
+        cardData.avatar_url = userData.avatar_url;
         cardData.img_url = '/asserts/image/home/content.png';
         //TODO： 要改user表的card里面
         await this.Model.add(cardData);
