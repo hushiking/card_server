dc9d24f69ac026203b2bfa15512294ea34299fc4
liuzn <liuzn@yunheit.com>
2018-03-17 19:35:04 +0800

    Add ali-oss

--- app/controller/card.js
+++ app/controller/card.js
@@ -1,15 +1,28 @@
 /**
  * Controller
  * @return
- */ 
-const {controller, helper} = require('thinkkoa');
+ */
+const { controller, helper } = require('thinkkoa');
 const cardModel = require('../model/card');
 const userModel = require('../model/user');
 const logicService = require('../service/common/logic');
 const admin_base = require('../common/admin_base.js');
+// 阿里云存储图片
+const fs = require('fs');
+const co = require('co');
+const path = require('path');
+const oss = require('ali-oss');
+
+//构建oss对象
+const store = oss({
+    accessKeyId: 'LTAISOc0ScI5UBmk',
+    accessKeySecret: 'xL0n36PhdE9i4xsRuHg8kkjzKVhcRT',
+    bucket: 'lzkj-card',
+    region: 'oss-cn-shanghai',
+});
 module.exports = class extends admin_base {
     //构造方法
-    init(ctx, app){
+    init(ctx, app) {
         //调用父类构造方法
         super.init(ctx, app);
         this.Model = new cardModel(this.app.config('config.model', 'middleware'));
@@ -22,26 +35,35 @@ module.exports = class extends admin_base {
         this.useModel = new userModel(this.app.config('config.model', 'middleware'));
     }
     //indexAction前置方法
-    _before_index(){
+    _before_index() {
         // console.log('_before_index');
     }
     //控制器默认方法
-    indexAction () {                                                                                                           
+    indexAction() {
         return this.ok('success');
     }
-    async saveCardAction () {
+
+    async saveCardAction() {
+        echo('success');
         let cardData = this.param();
-        let openid = this.param('openid');
-        let userData = await this.useModel.where({openid: openid}).find();
-        cardData.support = [];
-        cardData.comment = [];
-        cardData.nickname = userData.nickname;
-        cardData.avatar_url = userData.avatar_url;
-        cardData.group = userData.group;
-        cardData.img_url = '/asserts/image/home/content.png';
-        //TODO： 要改user表的card里面
-        await this.Model.add(cardData);
-        return this.ok('success');
+        let file = this.file();
+
+        co(async function(stream) {
+            let result = await store.putStream('images', fs.createReadStream(file));
+            // let result = await store.put('object-key', new Buffer('hello world'));
+        }).catch( (err) => {
+            echo(err);
+        })
+        // let userData = await this.useModel.where({ openid: this._userInfo.openid }).find();
+        // cardData.support = [];
+        // cardData.comment = [];
+        // cardData.nickname = userData.nickname;
+        // cardData.avatar_url = userData.avatar_url;
+        // cardData.group = userData.group;
+        // cardData.img_url = '/asserts/image/home/content.png';
+        // //TODO： 要改user表的card里面
+        // await this.Model.add(cardData);
+        // return this.ok('success');
     }
     async getListAction() {
         this.Mo.page = this.param('page') || 1;
@@ -50,13 +72,13 @@ module.exports = class extends admin_base {
     }
     async getUserCardAction() {
         let openid = this.param('openid');
-        let data = await this.Model.where({openid: openid}).select();
+        let data = await this.Model.where({ openid: openid }).select();
         echo(data);
         return this.ok('success', data);
     }
     async curentAction() {
         let id = this.param('id');
-        let data = await this.Model.where({id: id}).find();
+        let data = await this.Model.where({ id: id }).find();
         return this.ok('success', data);
     }
     // 点赞
@@ -67,18 +89,18 @@ module.exports = class extends admin_base {
         let userData = await this.useModel.where({ openid: openid }).find().catch(e => this.error(e.message))
         // console.log(userData)
         // console.log(data)
-        if(data.support.indexOf(userData.id) === -1){
+        if (data.support.indexOf(userData.id) === -1) {
             data.support.push(userData.id);
-            await this.Model.where({ id: id }).update({support: data.support}).catch(e => this.error(e.message));
-            return this.ok('success',{status: 1})
-        }else {
-            return this.ok('success',{status: 0})
+            await this.Model.where({ id: id }).update({ support: data.support }).catch(e => this.error(e.message));
+            return this.ok('success', { status: 1 })
+        } else {
+            return this.ok('success', { status: 0 })
         }
-        
+
     }
     async deleteAction() {
         let id = this.param('id');
-        await this.Model.where({id: id}).delete();
+        await this.Model.where({ id: id }).delete();
         return this.ok('delete success');
     }
 };
