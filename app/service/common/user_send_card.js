33724252333bb04c5f477d2377b978de4216bb5a
liuzn <liuzn@yunheit.com>
2018-04-18 09:33:34 +0800

    Fix： 修改设置用户使用次数时间轮训

--- app/service/common/user_send_card.js
+++ app/service/common/user_send_card.js
@@ -16,23 +16,29 @@ module.exports = class extends controller {
      * 
      */
     async reSetUserCardTimes(times, type = 'all', multiSelectArr) {
+        let send_card_power_status;
+        if(Number(times)){
+            send_card_power_status = 1;
+        }else{
+            send_card_power_status = 0;
+        }
         switch (type) {
             case 'all':
                 {
                     let userList = await this.userModel.where().select().catch(e => { });
                     let promiseList = [];
                     userList.forEach((item) => {
-                        promiseList.push(this.userModel.where({ id: item.id }).update({send_card_times: times}).catch(e => this.error(e.message)));
+                        promiseList.push(this.userModel.where({ id: item.id }).update({send_card_times: times, send_card_power: send_card_power_status}).catch(e => this.error(e.message)));
                     });
                 }
                 break;
             case 'multiSelect':
                 { 
-                    
                     let userList = await this.userModel.where({ id: multiSelectArr }).select().catch(e => { });
                     let promiseList = [];
+                 
                     userList.forEach((item) => {
-                        promiseList.push(this.userModel.where({ id: item.id }).update({send_card_times: times}).catch(e => this.error(e.message)));
+                        promiseList.push(this.userModel.where({ id: item.id }).update({send_card_times: times, send_card_power: send_card_power_status}).catch(e => this.error(e.message)));
                     });
                     Promise.all(promiseList).then(function (values) {
                         // console.log(values);
